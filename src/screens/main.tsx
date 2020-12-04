037480b96e0be778674d53f15ddfd2ed4aa3dd72
uggi <uggie.the.developer@gmail.com>
2020-12-04 14:50:58 +0100

    added chart

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Color } from '../colors';
 import CheerFeed from '../components/CheerFeed';
+import PieChart from '../components/PieChart';
 
 const MainScreen = () => {
   return (
     <div style={{ flex: 1, display: 'flex', flexDirection: 'column', backgroundColor: Color.Charcoal }}>
-      <div style={{ flex: 2, display: 'flex'}}>
-        
+      <div style={{ flex: 2, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <PieChart
+          width={600} height={600}
+          data={[10, 20, 70]}
+        />
       </div>
       <div style={{ flex: 1, display: 'flex'}}>
         <CheerFeed />
