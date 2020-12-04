b3afa563ec42d7bbc193c47c3186a8c618151b09
uggi <uggie.the.developer@gmail.com>
2020-12-04 12:02:58 +0100

    first changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Color } from './colors';
+
+const styles = {
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: Color.Charcoal,
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+
     </div>
   );
 }
---
+++ src/colors.ts
@@ -0,0 +1,44 @@
+export enum Color {
+  // Primary
+  Primary = '#FA4616',
+  PrimaryBrighter = '#FF5F34',
+  PrimaryDarker = '#D53C13',
+
+  // Charcoal
+  Charcoal = '#212222',
+  CharcoalBrighter = '#343434',
+  CharcoalDarker = '#000000',
+
+  // White
+  White = '#FFFFFF',
+  WhiteOff = '#F2F0F0',
+  WhiteDarker = '#919191',
+
+  // Buy Blue
+  BuyBlue = '#0071CD',
+  BuyBlueBrighter = '#178CEB',
+  BuyBlueDarker = '#0060AD',
+
+  // Green
+  Green = '#0E9F6D',
+  GreenBrighter = '#13B27B',
+  GreenDarker = '#0C8159',
+
+  // Yellow
+  Yellow = '#E6B52F',
+  YellowBrighter = '#FFCB3E',
+  YellowDarker = '#D8A720',
+
+  // Others
+  Black = 'black',
+  Black85 = '#000000D9',
+  WhiteSmoke = 'whitesmoke',
+  DarkGrey = '#212222',
+  DarkGrey90 = '#212222E6',
+  LightGrey90 = '#343434',
+  LightGrey = '#919191',
+  LightGrey10 = '#F2F0F0',
+  Primary10 = '#FA46160D',
+  FacebookBlue = '#5890FF',
+  Transparent = '#FFFFFF00',
+}
