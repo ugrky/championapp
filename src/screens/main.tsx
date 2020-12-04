50a23ca41ce8371aec77a88fd4199de4410b3966
David Bjerremose <dab@davidbjerremose.com>
2020-12-04 15:11:38 +0100

    Moved to value wheel container

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { useFirebaseConnect } from 'react-redux-firebase';
 import { Color } from '../colors';
 import CheerFeed from '../components/CheerFeed';
-import PieChart from '../components/PieChart';
+import ValueWheelContainer from '../containers/ValueWheel';
 
 const MainScreen = () => {
-
-  useFirebaseConnect([
-    'cheers'
-  ]);
-
-  const cheers = useSelector((state: any) => state.firebase.ordered.cheers);
-
   return (
     <div style={{ flex: 1, display: 'flex', flexDirection: 'column', backgroundColor: Color.Charcoal }}>
       <div style={{ flex: 2, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <PieChart
-          width={600} height={600}
-          data={[10, 20, 70]}
-        />
+        <ValueWheelContainer />
       </div>
       <div style={{ flex: 1, display: 'flex'}}>
         <CheerFeed />
