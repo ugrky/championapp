9b8b718852c2b37fc2caf374d79f8e0385e1cb80
David Bjerremose <dab@davidbjerremose.com>
2020-12-04 12:52:10 +0100

    Added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { Color } from './colors';
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-const styles = {
-  container: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: Color.Charcoal,
-  }
-}
+import { Color } from './colors';
+import firebase from 'firebase';
+import { firebaseConfig } from './configuration/firebase';
 
 function App() {
-  return (
-    <div style={styles.container}>
 
-    </div>
+  // Initialize firebase config
+  useEffect(() => {
+    firebase.initializeApp(firebaseConfig);
+  }, []);
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login"></Route>
+        <Route path="/submit"></Route>
+        <Route path="/"></Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/configuration/firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD3MsXnWsT8YtXRU89PRRyW5XhytLhbY58",
+  authDomain: "soundboks-abc.firebaseapp.com",
+  databaseURL: "https://soundboks-abc-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "soundboks-abc",
+  storageBucket: "soundboks-abc.appspot.com",
+  messagingSenderId: "184768808643",
+  appId: "1:184768808643:web:28f7ec9eb1a55fd5040476"
+};
